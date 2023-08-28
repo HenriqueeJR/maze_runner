04cba814a1250816a8634d057df8ebe22bd3f627
HenriqueeJR <91606600+HenriqueeJR@users.noreply.github.com>
2023-08-28 16:12:34 +0000

    VERSAO FINAL DE FATO

--- maze_runner.cpp
+++ maze_runner.cpp
@@ -18,21 +18,6 @@ struct pos_t {
 // Estrutura de dados contendo as próximas
 // posicões a serem exploradas no labirinto
 std::stack<pos_t> valid_positions;
-/* Inserir elemento: 
-
-	 pos_t pos;
-	 pos.i = 1;
-	 pos.j = 3;
-	 valid_positions.push(pos)
- */
-// Retornar o numero de elementos: 
-//    valid_positions.size();
-// 
-// Retornar o elemento no topo: 
-//  valid_positions.top(); 
-// 
-// Remover o primeiro elemento do vetor: 
-//    valid_positions.pop();
 
 
 // Função que le o labirinto de um arquivo texto, carrega em 
@@ -84,35 +69,9 @@ void print_maze() {
 // Recebe como entrada a posição initial e retorna um booleando indicando se a saída foi encontrada
 bool walk(pos_t pos) {
 
-	
-	// Repita até que a saída seja encontrada ou não existam mais posições não exploradas
-		// Marcar a posição atual com o símbolo '.'
-		// Limpa a tela
-		// Imprime o labirinto
-		
-		/* Dado a posição atual, verifica quais sao as próximas posições válidas
-			Checar se as posições abaixo são validas (i>0, i<num_rows, j>0, j <num_cols)
-		 	e se são posições ainda não visitadas (ou seja, caracter 'x') e inserir
-		 	cada uma delas no vetor valid_positions
-		 		- pos.i, pos.j+1
-		 		- pos.i, pos.j-1
-		 		- pos.i+1, pos.j
-		 		- pos.i-1, pos.j
-		 	Caso alguma das posiçÕes validas seja igual a 's', retornar verdadeiro
-	 	*/
-
-		
-	
-		// Verifica se a pilha de posições nao esta vazia 
-		//Caso não esteja, pegar o primeiro valor de  valid_positions, remove-lo e chamar a funçao walk com esse valor
-		// Caso contrario, retornar falso
-
 	maze[pos.i][pos.j] = '.';
 	system("clear");
-
-	//printf("i:%d j:%d \n", pos.i, pos.j);
 	print_maze();
-	//printf("\n\n\n");
 	
 	if (pos.i >= 0 && pos.i < num_rows && (pos.j+1) >= 0 && (pos.j+1) < num_cols) {
 		if (maze[pos.i][pos.j+1] == 'x') {
@@ -159,7 +118,6 @@ bool walk(pos_t pos) {
 	}
 
 
-	
 	if (!valid_positions.empty()) {
 		pos_t next_position = valid_positions.top();
 		valid_positions.pop();
@@ -169,8 +127,10 @@ bool walk(pos_t pos) {
 }
 
 int main(int argc, char* argv[]) {
+
 	// carregar o labirinto com o nome do arquivo recebido como argumento
 	pos_t initial_pos = load_maze(argv[1]);
+
 	// chamar a função de navegação
 	bool exit_found = walk(initial_pos);
 
@@ -178,7 +138,6 @@ int main(int argc, char* argv[]) {
 	else printf("%s\n", "SAIDA NAO ENCONTRADA");
 	
 	
-	// Tratar o retorno (imprimir mensagem)
 	
 	return 0;
 }
